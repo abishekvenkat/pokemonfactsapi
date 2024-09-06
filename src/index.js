79c0bcf6696bd7d9f46958263444cfbdd3d584c1
abishekvenkat <86376376+abishekvenkat@users.noreply.github.com>
2024-09-06 23:13:59 +0530

    added facts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const facts = require('./pokemonFacts.json');
+const generations = require('./pokemonGenerations.json');
 
 const cors = require('cors');
 
@@ -11,20 +12,34 @@ app.use(cors());
 app.get('/', (req, res) => {
   const count = parseInt(req.query.count) || 1;
   const id = parseInt(req.query.id);
+  const gen = parseInt(req.query.gen);
 
   let selectedFacts;
 
   if (id !== undefined && !isNaN(id) && id >= 0 && id < facts.length) {
     selectedFacts = [facts[id]];
   } else {
-    selectedFacts = getRandomFacts(count);
+    selectedFacts = getRandomFacts(count, gen);
   }
 
   res.json({ data: selectedFacts });
 });
 
-function getRandomFacts(count) {
-  const shuffled = facts.sort(() => 0.5 - Math.random());
+function getRandomFacts(count, gen) {
+  let filteredFacts = facts;
+  
+  if (gen === undefined || isNaN(gen)) {
+    gen = Math.floor(Math.random() * 3) + 1; // Random number between 1 and 3
+  }
+
+  filteredFacts = facts.filter(fact => {
+    const pokemonNames = Object.keys(generations);
+    return pokemonNames.some(name => {
+      return fact.includes(name) && generations[name] === gen;
+    });
+  });
+
+  const shuffled = filteredFacts.sort(() => 0.5 - Math.random());
   return shuffled.slice(0, count);
 }
 
